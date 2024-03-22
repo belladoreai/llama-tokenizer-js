c0d6173afceea1ea266363ab4cccb9a0a7c46bf1
Shubham Tiwari <72434294+blaze2004@users.noreply.github.com>
2024-03-22 18:45:35 +0000

    fixed nextjs static export basepath

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig={
     output: 'export',
+    basePath: '/llama-tokenizer-js',
     reactStrictMode: true,
     transpilePackages: ["@repo/ui", "@repo/llama-tokenizer"]
 };
