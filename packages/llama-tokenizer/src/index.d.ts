7036838460b10f183c387006dc975fdf3c1d8170
Shubham Tiwari <72434294+blaze2004@users.noreply.github.com>
2024-03-23 16:21:13 +0000

    merged tests into the main tokenizer file

--- packages/llama-tokenizer/src/index.d.ts
+++ packages/llama-tokenizer/src/index.d.ts
@@ -18,6 +18,7 @@ export declare class LlamaTokenizer {
     constructor(vocab_base64?: string, merges_binary?: string);
     encode(prompt: string, add_bos_token?: boolean, add_preceding_space?: boolean, log_performance?: boolean): number[];
     decode(tokenIds: number[], add_bos_token?: boolean, add_preceding_space?: boolean): string;
+    runTests(tests?: (tokenizer: LlamaTokenizer) => boolean): void
 }
 declare const llamaTokenizer: LlamaTokenizer;
 export default llamaTokenizer;
